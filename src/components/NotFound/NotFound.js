7781ce6b072e0bf178d3bfdfda2c6b9a6670c08f
sharif764 <ss1884088@gmail.com>
2021-10-04 22:29:39 +0600

    j

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h1>This is Notfound</h1>
+            <h1 className="text-center text-danger">Not Found !!! <br />Aikane Kicu nai !!! <br />Ai name Kono Page ba Route nai !!!</h1>
         </div>
     );
 };
