2a313017e7ecbc1822ae1a1c6f66cf3a174e893a
sharif764 <ss1884088@gmail.com>
2021-10-04 20:55:44 +0600

    J

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,9 +1,48 @@
 import React from 'react';
-
+import './AboutUs.css';
 const AboutUs = () => {
     return (
-        <div>
-            <h1>This is about  us</h1>
+        <div className="about-div">
+            <h1 className="text-center">About Us</h1>
+            <div className="d-flex passage-container">
+                <div className="passage">
+                    <h1>Our Vision</h1>
+                    <p>Our vision is a world of vibrant, multi-generational communities in which each life stage and the transitions between them are honored, in which all people are guided to recognize and give their gifts in service to a thriving planet. Find out more about our Mission, History, Locations and Staff here →</p>
+                </div>
+                <div className="passage">
+                    <h1>Our Mission</h1>
+                    <p>Rite of Passage Journeys, established in 1968, fosters self-discovery, community belonging, and connection with the natural world. Through contemporary rite of passage experiences and education, we mentor youth, adults and elders through life transitions, initiating soulful leaders for the next generation</p>
+                </div>
+                <div className="passage">
+                    <h1>Our Awards</h1>
+                    <p>
+                        The course of my life has been permanently altered, in the most precious and meaningful ways. I am better equipped to serve my community from my heart and I am better able to see the humanity in myself and others.”
+                    </p>
+                </div>
+            </div>
+            <div>
+                <h1 className="text-center my-5">Review</h1>
+                <div className="Reviewpassage-div">
+                    <div className="Reviewpassage">
+
+                        <h3>By Ritvik A:</h3>
+                        <br />This course was short but very informative and very helpful for an aspiring leader like myself. It also helped me understand how to view or understand when I receive feedback. I highly recommend it!Excellent course. I started it being a bit skeptical, just with the intention to go quickly through some video scripts. but I changed my mind. There is so much to learn here!
+                    </div>
+                    <div className="Reviewpassage">
+                        <h3>By Jhonson C:</h3>
+                        <br />
+                        Excellent course. I started it being a bit skeptical, just with the intention to go quickly through some video scripts. but I changed my mind. There is so much to learn here.Excellent course. I started it being a bit skeptical, just with the intention to go quickly through some video scripts. but I changed my mind. There is so much to learn here
+                    </div>
+                    <div className="Reviewpassage">
+                        <h3>By Steve C:</h3>
+                        <br />Excellent course. I started it being a bit skeptical, just with the intention to go quickly through some video scripts. but I changed my mind. There is so much to learn here.Excellent course. I started it being a bit skeptical, just with the intention to go quickly through some video scripts. but I changed my mind. There is so much to learn here
+                    </div>
+                    <div className="Reviewpassage">
+                        <h3>By Bruno C:</h3>
+                        <br />Excellent course. I started it being a bit skeptical, just with the intention to go quickly through some video scripts. but I changed my mind. There is so much to learn here.Excellent course. I started it being a bit skeptical, just with the intention to go quickly through some video scripts. but I changed my mind. There is so much to learn here
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
